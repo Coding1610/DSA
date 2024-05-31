2c96c9ee611daeb2006e1ece599933b1be7c7adf
yp_016 <yp.70010@gmail.com>
2024-05-31 22:33:33 +0530

    Add Question

--- Array/Q.17_Rotate_An_Array.cpp
+++ Array/Q.17_Rotate_An_Array.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 /*
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {  
 
+        vector<int> temp(nums.size());
+        
+        for( int i = 0 ; i < nums.size() ; i++ ){
+            temp[(i+k)%nums.size()] = nums[i];
+        }
+        nums = temp;
+    }
+};
 */
