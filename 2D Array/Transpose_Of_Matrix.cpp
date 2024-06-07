5407a83744a07be8b66334c7860ceb26a50d678b
yp_016 <yp.70010@gmail.com>
2024-06-07 18:06:59 +0530

    Add Questions

---
+++ 2D Array/Transpose_Of_Matrix.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix){
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        vector<vector<int>> newMatrix( col , vector<int>(row));
+
+        for( int i = 0 ; i < col ; i++ ){
+            for( int j = 0 ; j < row ; j++ ){
+                newMatrix[i][j] = matrix[j][i];
+            }
+        }
+        return newMatrix;
+    }
+};
+*/
