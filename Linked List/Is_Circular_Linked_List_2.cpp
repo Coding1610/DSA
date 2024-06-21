11789812a1ab2cecc0dd6812c0a15c6c34e8f133
yp_016 <yp.70010@gmail.com>
2024-06-21 22:55:02 +0530

    Add Questions

---
+++ Linked List/Is_Circular_Linked_List_2.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+/*
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        if (!head) return NULL;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while( fast && fast -> next ){
+
+            slow = slow -> next;
+            fast = fast -> next -> next;
+
+            if( slow == fast ){
+                slow = head;
+                while( slow != fast ){
+                    slow = slow -> next;
+                    fast = fast -> next;
+                }
+                return slow;
+            }
+        }
+        return NULL;   
+    }
+};
+
+*/
