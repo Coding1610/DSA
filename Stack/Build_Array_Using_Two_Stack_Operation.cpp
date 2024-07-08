2266a90232ea9a5eedd7c3459f24fd8634898077
yp_016 <yp.70010@gmail.com>
2024-07-08 10:02:19 +0530

    Add Questions

---
+++ Stack/Build_Array_Using_Two_Stack_Operation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n){
+
+        int index = 0;
+        vector<string> ans;
+
+        for( int i = 1 ; i <= n && index < target.size() ; i++ ){
+            
+            ans.push_back("Push");
+
+            if( i != target[index] ){
+                ans.push_back("Pop");
+            }
+
+            else{
+                index++;
+            }
+
+        }
+        return ans;
+    }
+};
+
+*/
