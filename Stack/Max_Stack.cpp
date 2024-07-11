c13321da8bc061954c06194472d4783a9e1ec0ad
yp_016 <yp.70010@gmail.com>
2024-07-11 12:32:58 +0530

    Add Questions

---
+++ Stack/Max_Stack.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+class MaxStack {
+public:
+
+    vector<pair<int,int>> arr;
+
+    MaxStack() {}
+    
+    void push(int val) {
+
+        pair<int,int> p;
+
+        if( arr.size() == 0 ){
+            p.first = val;
+            p.second = val;
+        }
+
+        else{
+            p.first = val;
+            p.second = max( val , arr.back().second );
+        } 
+
+        arr.push_back(p);
+
+    }
+    
+    void pop(){
+        arr.pop_back();
+    }
+    
+    int top(){
+        return arr.back().first;
+    }
+    
+    int getMax() {
+        return arr.back().second;
+    }
+    
+};
+
+*/
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
