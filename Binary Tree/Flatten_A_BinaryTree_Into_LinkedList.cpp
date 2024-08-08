d0b859cdfc8de3b9159e4039560cbc4686ec234d
yp_016 <yp.70010@gmail.com>
2024-08-08 11:27:23 +0530

    Add Questions

--- Binary Tree/Flatten_A_BinaryTree_Into_LinkedList.cpp
+++ Binary Tree/Flatten_A_BinaryTree_Into_LinkedList.cpp
@@ -3,6 +3,36 @@ using namespace std;
 
 /*
 
+class Solution {
+public:
+    void flatten(TreeNode* root) {
 
+        TreeNode* curr = root;
+
+        while( curr ){
+
+            if( curr -> left ){
+
+                TreeNode* pred = curr -> left;
+
+                while( pred -> right ){
+                    pred = pred -> right;
+                } 
+
+                pred -> right = curr -> right;
+                curr -> right = curr -> left;
+                curr -> left = NULL;
+
+            }
+            
+            else{
+                curr = curr -> right;
+            }
+
+        }
+        
+    }
+
+};
 
 */
