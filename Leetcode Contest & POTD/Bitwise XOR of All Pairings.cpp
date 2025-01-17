d5b25e7c80337b7b94208234f177bad3b9ffe04f
Coding1610 <yp.70010@gmail.com>
2025-01-17 10:56:51 +0530

    Added Questions

---
+++ Leetcode Contest & POTD/Bitwise XOR of All Pairings.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2){
+
+        int x1 = 0;
+        int x2 = 0;
+
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+
+        if( len1 % 2 != 0 ){
+            for( int num : nums2 ){
+                x2 ^= num;
+            }
+        }
+
+        if( len2 % 2 != 0 ){
+            for( int num : nums1 ){
+                x1 ^= num;
+            }
+        }
+
+        return x1 ^ x2;
+        
+    }
+};
+
+*/
---
+++ Leetcode Contest & POTD/Neighboring Bitwise XOR.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        
+        int x = 0;
+        
+        for( int bit : derived ){
+            x ^= bit;
+        }
+
+        return ( x == 0 ? true : false );
+    
+    }
+};
+
+*/
