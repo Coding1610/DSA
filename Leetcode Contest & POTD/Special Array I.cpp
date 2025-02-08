c3f1ed87a34dbb4babf5719ecaa7e758f31eeff1
Coding1610 <yp.70010@gmail.com>
2025-02-08 10:58:53 +0530

    Added POTD Feb 1-2-25

--- Leetcode Contest & POTD/Special Array I.cpp
+++ Leetcode Contest & POTD/Special Array I.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         for(int i = 1; i < nums.size(); ++i) {
-            if(nums[i - 1] % 2 == nums[i] % 2) {
+            if(nums[i-1] % 2 == nums[i] % 2) {
                 return false;
             }
         }    
