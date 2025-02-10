90c03a04ef74ab5b6243c5ceb92acdf28fd48f4b
Coding1610 <yp.70010@gmail.com>
2025-02-10 19:02:27 +0530

    Added POTS feb 10-2-25

---
+++ Leetcode Contest & POTD/Clear Digit.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+class Solution {
+public:
+    string clearDigits(string s){
+ 
+        string newS = "";
+ 
+        for( char ch : s ){
+            if( isdigit(ch) ){
+                if( !newS.empty() ) newS.pop_back();
+            }
+            else{
+                newS.push_back(ch);
+            }
+        }
+
+        return newS;
+        
+    }
+};
+
+*/
