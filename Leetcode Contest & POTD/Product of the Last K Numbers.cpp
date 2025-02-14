e8816f13847e0d428a7efb780f4cf2879428b143
Coding1610 <yp.70010@gmail.com>
2025-02-14 14:09:21 +0530

    Added POTD feb 14-2-15

---
+++ Leetcode Contest & POTD/Product of the Last K Numbers.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+class ProductOfNumbers {
+public:
+
+    vector<int> arr;
+
+    ProductOfNumbers() {}
+    
+    void add(int num){
+        arr.push_back(num);
+    }
+    
+    int getProduct(int k){
+
+        long long int ans = 1;
+        int i = arr.size()-1;
+
+        while( k != 0 ){
+            ans *= arr[i];
+            k--;
+            i--;
+        }
+
+        return ans;
+        
+    }
+};
+
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+
+*/
